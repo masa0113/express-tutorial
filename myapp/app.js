3ccc30926b87815b3246e9cc01af3543a0be4390
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-01-19 22:39:48 +0900

    commit install csv-parser

--- myapp/app.js
+++ myapp/app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var resultRouter = require('./routes/result');
 
 var app = express();
+var helloworldRouter = require('./routes/helloworld');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/helloworld', helloworldRouter);
+app.use('/result', resultRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
