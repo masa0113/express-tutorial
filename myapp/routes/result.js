bf889fe1b7fdc48a5cbbad6b53ff33c817065c68
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-01-19 22:48:13 +0900

    commit refactoring

--- myapp/routes/result.js
+++ myapp/routes/result.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var load_csv = require('../db/load_csv.js')
+var load_csv = require('../data/load_csv.js')
 
 
 var inputFilePath = './data/jukenresult.csv'
